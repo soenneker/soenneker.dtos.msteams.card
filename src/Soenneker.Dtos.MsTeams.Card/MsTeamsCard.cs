72853695c91688795d2995c915b47c444c5ba529
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:25 -0500

    xml commenting

--- src/Soenneker.Dtos.MsTeams.Card/MsTeamsCard.cs
+++ src/Soenneker.Dtos.MsTeams.Card/MsTeamsCard.cs
@@ -10,10 +10,16 @@ namespace Soenneker.Dtos.MsTeams.Card;
 /// </summary>
 public class MsTeamsCard
 {
+    /// <summary>
+    /// Gets or sets type.
+    /// </summary>
     [JsonProperty("type")]
     [JsonPropertyName("type")]
     public string Type { get; set; } = null!;
 
+    /// <summary>
+    /// Gets or sets attachments.
+    /// </summary>
     [JsonProperty("attachments")]
     [JsonPropertyName("attachments")]
     public List<AdaptiveCardAttachments> Attachments { get; set; } = null!;
